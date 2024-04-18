97c4dea8f38f525741fb5ca6f2bb33cfc30764f4
Christoph Koch <christoph.koch@epfl.ch>
2024-04-18 17:49:22 +0200

    .

---
+++ code/actuators/SimpleFOC/B_G431B_ESC1_x2212_angle/B_G431B_ESC1_x2212_angle.ino
@@ -0,0 +1,62 @@
+#include <SimpleFOC.h>
+
+BLDCMotor motor = BLDCMotor(7, 0.092); // x2212
+BLDCDriver6PWM driver = BLDCDriver6PWM(A_PHASE_UH, A_PHASE_UL, A_PHASE_VH, A_PHASE_VL, A_PHASE_WH, A_PHASE_WL);
+LowsideCurrentSense currentSense = LowsideCurrentSense(0.003f, -64.0f/7.0f, A_OP1_OUT, A_OP2_OUT, A_OP3_OUT);
+Encoder encoder = Encoder(A_HALL2, A_HALL3, 4096); //TLE5012b
+
+void doA(){encoder.handleA();}
+void doB(){encoder.handleB();}
+
+Commander command = Commander(Serial);
+void doTarget(char* cmd) { command.motion(&motor, cmd); }
+
+void setup() {
+
+  encoder.init();
+  encoder.enableInterrupts(doA, doB); 
+  motor.linkSensor(&encoder);
+  
+  driver.voltage_power_supply = 12;
+  driver.voltage_limit = 12;
+  driver.init();
+  motor.linkDriver(&driver);
+
+  currentSense.linkDriver(&driver);
+  currentSense.init();
+  currentSense.skip_align = true;
+  motor.linkCurrentSense(&currentSense);
+
+  motor.controller = MotionControlType::angle;
+
+  motor.voltage_limit = 12;
+  motor.current_limit = 15;
+  motor.velocity_limit = 1000;
+
+  motor.PID_velocity.P = 0.2;
+  motor.PID_velocity.I = 20;
+  motor.PID_velocity.D = 0; // optional?
+  motor.PID_velocity.output_ramp = 1000;
+  motor.LPF_velocity.Tf = 0.01;
+  motor.P_angle.P = 20;
+
+  motor.target = 0;
+
+  Serial.begin(115200);
+  motor.useMonitoring(Serial);
+  
+  motor.init();
+  motor.initFOC();
+
+  command.add('T', doTarget, "target angle");
+
+  Serial.println(F("Motor ready."));
+  Serial.println(F("Set the target angle using serial terminal:"));
+  _delay(1000);
+}
+
+void loop() {
+  motor.loopFOC();
+  motor.move();
+  command.run();
+}

07106f01f66be3b2718959b084119a3a2c168805
Christoph Koch <christoph.koch@epfl.ch>
2024-11-06 14:33:32 +0100

    .

---
+++ code/actuators/odrive_v3.6/odrv_hobbystar540_13_5.py
@@ -0,0 +1,66 @@
+# hobbystar 540  13.5 turns  1pp, 3050kv ?
+
+#dump_errors(odrv0)
+
+odrv0.reboot()
+odrv0.erase_configuration()
+
+odrv0.config.brake_resistance = 2
+odrv0.config.dc_max_positive_current = 18
+
+odrv0.axis0.requested_state = AXIS_STATE_IDLE
+odrv0.axis0.encoder.config.pre_calibrated = False
+odrv0.axis0.motor.config.pre_calibrated = False
+odrv0.axis0.config.startup_encoder_index_search = False
+
+odrv0.axis0.controller.config.vel_limit = 20
+odrv0.axis0.motor.config.pole_pairs = 1
+odrv0.axis0.motor.config.torque_constant = 8.27 / 3050
+odrv0.axis0.motor.config.current_lim = 49 # max continuous current
+odrv0.axis0.motor.config.calibration_current = 20
+odrv0.axis0.config.calibration_lockin.current = 20
+odrv0.axis0.encoder.config.cpr = 8192
+
+odrv0.save_configuration()
+
+
+odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+#wait until finished before issuing more commands
+
+odrv0.axis0.encoder.config.use_index = False
+#odrv0.axis0.encoder.config.use_index = True
+#odrv0.axis0.requested_state = AXIS_STATE_ENCODER_INDEX_SEARCH
+#wait until finished before issuing more commands
+
+odrv0.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
+#wait until finished before issuing more commands
+
+odrv0.axis0.encoder.config.pre_calibrated = True
+odrv0.axis0.motor.config.pre_calibrated = True
+#odrv0.axis0.config.startup_encoder_index_search = True
+odrv0.save_configuration()
+
+
+# tuning, was done with 50V
+
+odrv0.axis0.controller.config.vel_integrator_gain = 0
+odrv0.axis0.controller.config.vel_gain = 0.4  # default 0.16666666
+odrv0.axis0.controller.config.pos_gain = 150  # default 20
+odrv0.axis0.controller.config.vel_integrator_gain = 0.5 * 10 * odrv0.axis0.controller.config.vel_gain
+odrv0.axis0.requested_state = AXIS_STATE_IDLE
+odrv0.save_configuration()
+
+
+
+# filtered position control
+odrv0.axis0.controller.config.input_filter_bandwidth = 2
+odrv0.axis0.controller.config.input_mode = INPUT_MODE_POS_FILTER
+odrv0.save_configuration()
+
+
+
+odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+odrv0.axis0.controller.input_pos = 1.5
+
+
+
